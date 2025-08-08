7046830aaecda33af523da7d10e80e75f10c8dde
HeinHtettt98 <heinhtet352002@gmail.com>
2025-08-08 22:57:01 +0630

    server

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("email");
+  res.send("email");
 });
 
 app.use("/user",userRoute);
